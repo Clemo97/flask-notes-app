67ad7a132e72338ebfe739b983636520c1a0a5ae
Clemo97 <lumumbaclement@gmail.com>
2022-05-14 21:38:06 +0300

    create sign up form html

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,12 @@
-from flask import Blueprint
+from xmlrpc.client import boolean
+from flask import Blueprint, render_template
+from matplotlib.pyplot import text
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
@@ -12,4 +14,4 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
