f3fe1ad580b9c5e9f7771e548dea8fd09c55f371
Clemo97 <lumumbaclement@gmail.com>
2022-05-15 15:53:03 +0300

    create sqlalchemy

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlaclchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "databse.db" 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'FRGHTED5Kinf45v2'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     from .views import views
     from .auth import auth
